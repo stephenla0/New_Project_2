fc1b46610260e36a6632ede82f4a92c15fc0c003
ailish skinner <aisk8069@colorado.edu>
2022-03-11 19:35:53 -0700

    Working on implementing JUnit Tests

--- src/com/OOAD/testing.java
+++ src/com/OOAD/testing.java
@@ -9,40 +9,97 @@ import static org.junit.Assert.*;
 // You must be able to run your code in test mode to run these JUnit tests and capture the output of the tests
 // to a test output text file in your repository.
 public class testing {
-
-    //instantiation of variables to test
-    CD cd = new CD();
-    Guitar gt = new Guitar();
-    Flute fl = new Flute();
-    Hats hat = new Hats();
-    GigBag gb = new GigBag();
-    Cables cable = new Cables();
-
-    public void testItems()
+    public void testMusic()
     {
-        //Basic testing
+        CD cd = new CD(); //1
         assertTrue(cd instanceof Item);
         assertTrue(cd instanceof Music);
-        assertTrue(fl instanceof Item);
-        assertTrue(hat instanceof Item);
-        assertTrue(gb instanceof Item);
-        assertTrue(gt instanceof Item);
-        assertTrue(cable instanceof Item);
+
+        PaperScore ps = new PaperScore(); //2
+        assertTrue(ps instanceof Item);
+        assertTrue(ps instanceof Music);
+
+        Vinyl vl = new Vinyl(); //3
+        assertTrue(vl instanceof Item);
+        assertTrue(vl instanceof Music);
+
+        Cassette cs = new Cassette(); //4
+        assertTrue(cs instanceof Item);
+        assertTrue(cs instanceof Music);
     }
 
-    public void testSimilarItems() //so make sure they are of the same class
+    public void testPlayers()
     {
-        //making sure classes are same
-        assertEquals(cable.getClass(), GigBag.class);
-        assertEquals(gt.getClass(), Stringed.class);
-        assertEquals(cd.getClass(), Vinyl.class);
-        assertEquals(hat.getClass(), Shirts.class);
-        assertEquals(fl.getClass(), Wind.class);
+        CDPlayer cd = new CDPlayer(); //5
+        assertTrue(cd instanceof Item);
+        assertTrue(cd instanceof Players);
+
+        RecordPlayer ps = new RecordPlayer(); //6
+        assertTrue(ps instanceof Item);
+        assertTrue(ps instanceof Players);
+
+        CassettePlayer vl = new CassettePlayer(); //7
+        assertTrue(vl instanceof Item);
+        assertTrue(vl instanceof Players);
+
+        MP3 cs = new MP3(); //8
+        assertTrue(cs instanceof Item);
+        assertTrue(cs instanceof Players);
+    }
+    public void testInstruments()
+    {
+        Guitar gt = new Guitar(); //8
+        assertTrue(gt instanceof Item);
+        assertTrue(gt instanceof Stringed);
+        assertTrue(gt instanceof Instrument);
+
+
+        Bass bass = new Bass(); //9
+        assertTrue(bass instanceof Item);
+        assertTrue(bass instanceof Stringed);
+        assertTrue(bass instanceof Instrument);
+
+        Mandolin mandolin = new Mandolin(); //10
+        assertTrue(mandolin instanceof Item);
+        assertTrue(mandolin instanceof Stringed);
+        assertTrue(mandolin instanceof Instrument);
+
+        Flute flute = new Flute(); //11
+        assertTrue(flute instanceof Item);
+        assertTrue(flute instanceof Wind);
+        assertTrue(flute instanceof Instrument);
+
+        Saxophone saxophone = new Saxophone(); //12
+        assertTrue(saxophone instanceof Item);
+        assertTrue(saxophone instanceof Wind);
+        assertTrue(saxophone instanceof Instrument);
+
+        Harmonica harmonica = new Harmonica(); //13
+        assertTrue(harmonica instanceof Item);
+        assertTrue(harmonica instanceof Wind);
+        assertTrue(harmonica instanceof Instrument);
+    }
+
+    public void testClothing()
+    {
+        Shirts shirt = new Shirts(); //15
+        assertTrue(shirt instanceof Item);
+        assertTrue(shirt instanceof Clothing);
+        assertTrue(shirt.size != null);
+
+        Hats hat = new Hats(); //16
+        assertTrue(hat instanceof Item);
+        assertTrue(hat instanceof Clothing);
+        assertTrue(hat.size != null);
+
+        Bandanas bandanas = new Bandanas(); //17
+        assertTrue(bandanas instanceof Item);
+        assertTrue(shirt instanceof Clothing);
     }
 
     public void testClerks()
     {
-        ClerkObserver observer = new ClerkObserver();
+        ClerkObserver observer = new ClerkObserver(); //14
         Store fakeStore = new Store();
         Clerk clerk = new Clerk("Tester Clerk", 0.4, fakeStore, observer,0.6);
         assertEquals(clerk.getClass(), Clerk.class);
